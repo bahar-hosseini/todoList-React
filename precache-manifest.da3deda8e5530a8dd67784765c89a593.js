c9ff5e66f9521e376750d5f5e304067c26b96471
Code Sandbox <hello@codesandbox.io>
2021-09-27 21:10:32 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.da3deda8e5530a8dd67784765c89a593.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a8b4b709fb5bc9547d5038ca2afca0",
+    "url": "https://bahar-hosseini.github.io/csb-6o6gc/index.html"
+  },
+  {
+    "revision": "a359d6f2db00b2f43600",
+    "url": "https://bahar-hosseini.github.io/csb-6o6gc/static/js/2.306c8396.chunk.js"
+  },
+  {
+    "revision": "0046fe9775006a583b20",
+    "url": "https://bahar-hosseini.github.io/csb-6o6gc/static/js/main.5edc520c.chunk.js"
+  },
+  {
+    "revision": "af68f7a1a963a7805dff",
+    "url": "https://bahar-hosseini.github.io/csb-6o6gc/static/js/runtime-main.ec28b1d9.js"
+  }
+]);
